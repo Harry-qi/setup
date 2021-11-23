9185bc1b296e17d433a1a3f37e8007ef9ac8cba3
chenmingqi <leiluo@startdt.com>
2021-11-23 16:44:56 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 打开微信
+open /Applications/WeChat.app
+
+# 打开钉钉
+open /Applications/DingTalk.app
+
+# 启动nginx
+nginx
+
+# 打开webstorm
+open /Applications/WebStorm.app 
+
+# 进入项目
+cd /Users/leiluo/Documents/project/simba
+
+# 启动项目
+npm run dev:local 
